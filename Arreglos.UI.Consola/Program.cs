eac8092ef7f8ee1ec1b804dc7cdc1dbc977f69c1
Esmelozano23 <25030149@itesa.edu.mc>
2026-09-22 08:26:36 -0600

    Version_02:Proyectos en blanco.

---
+++ Arreglos.UI.Consola/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\nArreglos");
+        //para que no aparezcan las letras
+        Console.ReadKey();
+    }
+}
